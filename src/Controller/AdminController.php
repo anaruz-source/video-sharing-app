5cb515b456b4c63d8efe83fb65565b704837082d
Abdelhaq KHARROU <abdelhaq.fste@gmail.com>
2021-09-10 17:55:32 +0100

    add twig admin template with admin area views and controller, installed webpackencore and orm

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/my_profile.html.twig');
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories(): Response
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+
+    /**
+     * @Route("/categories/edit", name="edit_category")
+     */
+    public function edit_category(): Response
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos(): Response
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    /**
+     * @Route("/upload", name="upload")
+     */
+    public function upload(): Response
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users(): Response
+    {
+        return $this->render('admin/users.html.twig');
+    }
+}
